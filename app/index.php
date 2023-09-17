c5f0ce35ba8072ad1548ea4a96d15fcfa4cec9c0
unaibermudez <unaberosa@gmail.com>
2023-09-17 13:50:24 +0200

    a

--- app/index.php
+++ app/index.php
@@ -1,23 +1,12 @@
-<?php
-session_start();
-header("X-Content-Type-Options: nosniff");
-header("X-Frame-Options: DENY");
-header('Content-Type: text/html; charset=utf-8');
-header("Content-Security-Policy: default-src 'self'; frame-ancestors 'none'; font-src fonts.gstatic.com 'unsafe-inline'; style-src 'self' fonts.googleapis.com 'unsafe-inline'; script-src 'self' 'unsafe-inline'; connect-src 'self';");
-
-// You can include additional PHP code here as needed.
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Car Dealership</title>
-    <link rel="icon" href="/images/car.ico">
-    
+    <!-- Your existing meta tags and headers -->
+    <!-- ... -->
+
     <!-- Include your stylesheets -->
-    <<link rel="stylesheet" href="/styles/styles.css">
+    <link rel="stylesheet" href="/styles/styles.css">
+    <link rel="stylesheet" href="/styles/nav-bar.css">
 
     <!-- Include any additional fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -25,13 +14,34 @@ header("Content-Security-Policy: default-src 'self'; frame-ancestors 'none'; fon
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
 </head>
 <body>
-    <!-- Include the navigation bar -->
-    
+    <!-- Navigation Bar -->
+    <nav>
+        <div class="nav-container">
+            <!-- Logo -->
+            <a href="/" class="logo">
+                <img src="/images/logo-motorcity.svg" alt="MotorCity Logo">
+            </a>
+
+            <!-- Left-side Menu (Catalog and About Us Buttons) -->
+            <div class="left-menu">
+                <a href="/catalog" class="button">Catalog</a>
+                <a href="/about" class="button">About Us</a>
+            </div>
+
+
+            <!-- Right-side Menu (Login and Register Buttons) -->
+            <div class="right-menu">
+                <a href="/login" class="button login-button">Login</a>
+                <a href="/register" class="button register-button">Register</a>
+            </div>
+        </div>
+    </nav>
+
 
     <!-- Define the structure of the homepage -->
     <section id="hero">
         <div id="hero-container">
-            <h1>Welcome to XYZ Car Dealership</h1>
+            <h1>Welcome to MotorCity Dealership</h1>
             <p>Your Destination for Quality Cars</p>
             <a href="/inventory" class="button">View Our Inventory</a>
         </div>
@@ -61,3 +71,4 @@ header("Content-Security-Policy: default-src 'self'; frame-ancestors 'none'; fon
     
 </body>
 </html>
+

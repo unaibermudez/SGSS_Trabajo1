dec23b071a9fbcb77b5978cb75377eccdaaedc32
Diego200202 <diegoeugenio61@gmail.com>
2023-11-15 16:55:35 +0100

    evitar XSS

--- app/registrarCoche.php
+++ app/registrarCoche.php
@@ -1,35 +1,58 @@
 <?php
 session_start();
-
 header('Content-Type: text/html; charset=utf-8');
 require('Database.php');
 $db = Database::getInstance();
 
+function sanitizeInput($data) {
+    // Utiliza filter_var con FILTER_SANITIZE_STRING para limpiar las cadenas
+    return filter_var($data, FILTER_SANITIZE_STRING);
+}
+
+function validateInt($data) {
+    // Validación específica para enteros
+    return filter_var($data, FILTER_VALIDATE_INT);
+}
+
 if(isset($_POST['submit'])){
     unset($_POST['submit']);
 
-        $datos['imagen'] = "/images/coche-predeterminado.webp";
-        $datos['marca'] = $_POST['marca'];
-        $datos['modelo'] = $_POST['modelo'];
-        $datos['precio'] = $_POST['precio'];
-        $datos['color'] = $_POST['color'];
-        $datos['kilometros'] = $_POST['kilometros'];
-        $datos['cambio'] = $_POST['cambio'];
-        $datos['anno'] = $_POST['anno'];
-        $datos['combustible'] = $_POST['combustible'];
-        $datos['caballos'] = $_POST['caballos'];
-        // Luego, almacena $password en la base de datos
-        
-        $error = $db->registrar_coche($datos);
+    // Sanitiza todos los datos de entrada
+    $datos['imagen'] = "/images/coche-predeterminado.webp";
+    $datos['marca'] = sanitizeInput($_POST['marca']);
+    $datos['modelo'] = sanitizeInput($_POST['modelo']);
+    $datos['precio'] = sanitizeInput($_POST['precio']);
+    $datos['color'] = sanitizeInput($_POST['color']);
+    $datos['kilometros'] = sanitizeInput($_POST['kilometros']);
+    $datos['cambio'] = sanitizeInput($_POST['cambio']);
 
-        if(!isset($error)){
-            header('Location:catalogo.php');
-        }
+    // Validar datos numéricos
+    $datos['anno'] = filter_var($_POST['anno'], FILTER_VALIDATE_INT);
+    $datos['caballos'] = filter_var($_POST['caballos'], FILTER_VALIDATE_INT);
 
-        // Hacemos algo con el error
-        echo $error;
+    // Verificar si la validación fue exitosa
+    if ($datos['anno'] === false || $datos['anno'] === null) {
+        // Manejar el error para 'anno'
+    }
+
+    if ($datos['caballos'] === false || $datos['caballos'] === null) {
+    // Manejar el error para 'caballos'
+    }
+
+
+    // Luego, almacena $password en la base de datos
+    $error = $db->registrar_coche($datos);
+
+    if(!isset($error)){
+        header('Location:catalogo.php');
+        exit();
+    }
+
+    // Hacemos algo con el error
+    echo $error;
 }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -37,7 +60,7 @@ if(isset($_POST['submit'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrar Coche</title>
-    <link rel="stylesheet" href="/styles/register.css">  <!-- Include your CSS file for styling -->
+    <link rel="stylesheet" href="/styles/register.css">  <!-- Incluye tu archivo CSS para el estilo -->
 </head>
 <body>
 
@@ -46,79 +69,74 @@ if(isset($_POST['submit'])){
 <div class="register-container">
     <h1>Añadir Coche</h1>
 
-    <form method="POST" action="" onsubmit="validar_y_enviar_coches()">
+    <form method="POST" action="" onsubmit="return validar_y_enviar_coches()">
 
         <div class="form-item">
             <label for="marca">Marca:</label>
-            <input type="text" name="marca" id="marca" >
+            <input type="text" name="marca" id="marca" value="<?php echo isset($_POST['marca']) ? htmlspecialchars($_POST['marca']) : ''; ?>">
             <span id="errorMarca" class="error"></span>
         </div>
 
         <div class="form-item">
             <label for="modelo">Modelo:</label>
-            <input type="text" name="modelo" id="modelo" >
+            <input type="text" name="modelo" id="modelo" value="<?php echo isset($_POST['modelo']) ? htmlspecialchars($_POST['modelo']) : ''; ?>">
             <span id="errorModelo" class="error"></span>
         </div>
 
         <div class="form-item">
             <label for="anno">Año:</label>
-            <input type="int" name="anno" id="anno" >
+            <input type="number" name="anno" id="anno" value="<?php echo isset($_POST['anno']) ? htmlspecialchars($_POST['anno']) : ''; ?>">
             <span id="errorAnno" class="error"></span>
         </div>
 
         <div class="form-item">
             <label for="color">Color:</label>
-            <input type="text" name="color" id="color" >
+            <input type="text" name="color" id="color" value="<?php echo isset($_POST['color']) ? htmlspecialchars($_POST['color']) : ''; ?>">
             <span id="errorColor" class="error"></span>
         </div>
 
         <div class="form-item">
             <label for="caballos">Caballos:</label>
-            <input type="text" name="caballos" id="caballos" >
+            <input type="number" name="caballos" id="caballos" value="<?php echo isset($_POST['caballos']) ? htmlspecialchars($_POST['caballos']) : ''; ?>">
             <span id="errorCaballos" class="error"></span>
         </div>
 
         <div class="form-item">
             <label for="combustible">Combustible:</label>
             <select name="combustible" id="combustible">
-                <option value="Gasolina" <?= $_SESSION["combustible"] === 'Gasolina' ? 'selected' : '' ?>>Gasolina</option>
-                <option value="Diesel" <?= $_SESSION["combustible"] === 'Diesel' ? 'selected' : '' ?>>Diesel</option>
-                <option value="Hibrido" <?= $_SESSION["combustible"] === 'Hibrido' ? 'selected' : '' ?>>Hibrido</option>
-                <option value="Electrico" <?= $_SESSION["combustible"] === 'Electrico' ? 'selected' : '' ?>>Electrico</option>
+                <option value="Gasolina" <?php echo (isset($_POST['combustible']) && $_POST['combustible'] === 'Gasolina') ? 'selected' : ''; ?>>Gasolina</option>
+                <option value="Diesel" <?php echo (isset($_POST['combustible']) && $_POST['combustible'] === 'Diesel') ? 'selected' : ''; ?>>Diesel</option>
+                <option value="Hibrido" <?php echo (isset($_POST['combustible']) && $_POST['combustible'] === 'Hibrido') ? 'selected' : ''; ?>>Híbrido</option>
+                <option value="Electrico" <?php echo (isset($_POST['combustible']) && $_POST['combustible'] === 'Electrico') ? 'selected' : ''; ?>>Eléctrico</option>
             </select>
         </div>
 
         <div class="form-item">
             <label for="precio">Precio:</label>
-            <input type="text" name="precio" id="precio" >
+            <input type="number" name="precio" id="precio" value="<?php echo isset($_POST['precio']) ? htmlspecialchars($_POST['precio']) : ''; ?>">
             <span id="errorPrecio" class="error"></span>
         </div>
 
         <div class="form-item">
             <label for="kilometros">Kilometros:</label>
-            <input type="text" name="kilometros" id="kilometros" >
+            <input type="number" name="kilometros" id="kilometros" value="<?php echo isset($_POST['kilometros']) ? htmlspecialchars($_POST['kilometros']) : ''; ?>">
             <span id="errorKilometros" class="error"></span>
         </div>
 
         <div class="form-item">
             <label for="cambio">Tipo de Cambio:</label>
             <select name="cambio" id="cambio">
-                <option value="Manual" <?= $_SESSION["cambio"] === 'Manual' ? 'selected' : '' ?>>Manual</option>
-                <option value="Automatico" <?= $_SESSION["cambio"] === 'Automatico' ? 'selected' : '' ?>>Automático</option>
+                <option value="Manual" <?php echo (isset($_POST['cambio']) && $_POST['cambio'] === 'Manual') ? 'selected' : ''; ?>>Manual</option>
+                <option value="Automatico" <?php echo (isset($_POST['cambio']) && $_POST['cambio'] === 'Automatico') ? 'selected' : ''; ?>>Automático</option>
             </select>
         </div>
-        <!--
-        <form action="process_form.php" method="post" enctype="multipart/form-data">
-            <label for="image">Select an Image:</label>
-            <input type="file" id="image" name="image" accept="image/*">
-            <input type="submit" value="Upload">
-        </form>
-        -->
 
-        <button id="button" type="submit" name="submit" onclick="validar_y_enviar_coches()">Añadir vehiculo</button>
+        <button id="button" type="submit" name="submit" onclick="return validar_y_enviar_coches()">Añadir vehiculo</button>
 
     </form>
 </div>
+
+<!-- Incluir tu script JavaScript -->
 <script defer src="scripts/forms2.js"></script>
 </body>
 </html>
--- app/scripts/forms2.js
+++ app/scripts/forms2.js
@@ -32,205 +32,104 @@ function validar_y_eliminar(){
 }
 
 
-function validar_y_enviar_coches(){
+function validar_y_enviar_coches(event) {
     // Tomamos los elementos del formulario
-    let marca    = document.getElementById('marca') 
-    let modelo = document.getElementById('modelo') 
-    let anno     = document.getElementById('anno') 
-    let color    = document.getElementById('color') 
-    let caballos   = document.getElementById('caballos') 
-    let combustible   = document.getElementById('combustible') 
-    let precio    = document.getElementById('precio') 
-    let kilometros    = document.getElementById('kilometros') 
-    let cambio     = document.getElementById('cambio')
-    let button  = document.getElementById('button') 
+    let marca = document.getElementById('marca');
+    let modelo = document.getElementById('modelo');
+    let anno = document.getElementById('anno');
+    let color = document.getElementById('color');
+    let caballos = document.getElementById('caballos');
+    let combustible = document.getElementById('combustible');
+    let precio = document.getElementById('precio');
+    let kilometros = document.getElementById('kilometros');
+    let cambio = document.getElementById('cambio');
+    let button = document.getElementById('button');
 
     // Tomamos los elementos de output para los errores
-    let errMarca  = document.getElementById('errorMarca') 
-    let errModelo      = document.getElementById('errorModelo') 
-    let errAnno    = document.getElementById('errorAnno') 
-    let errColor    = document.getElementById('errorColor') 
-    let errCaballos     = document.getElementById('errorCaballos')
-    let errPrecio     = document.getElementById('errorPrecio') 
-    let errKilometros     = document.getElementById('errorKilometros') 
+    let errMarca = document.getElementById('errorMarca');
+    let errModelo = document.getElementById('errorModelo');
+    let errAnno = document.getElementById('errorAnno');
+    let errColor = document.getElementById('errorColor');
+    let errCaballos = document.getElementById('errorCaballos');
+    let errPrecio = document.getElementById('errorPrecio');
+    let errKilometros = document.getElementById('errorKilometros');
     let server = window.location.href;
-    let error=0;
+    let error = 0;
     let data = {};
     let type;
 
     // Comprobamos el tipo de formulario (log in, register...)
-    if(button){
+    if (button) {
         type = button.name;
-    }else{
+    } else {
         console.error("ERROR: el formulario no tiene un tipo")
-        return
+        return;
     }
 
-    data[type] = true
+    data[type] = true;
 
     // Comprobamos los campos y los validamos
-    if(marca){
-        if(marca.value==""){
-            //si nombre vacio nombre incorrecto
-            if(errMarca){
-                errMarca.innerHTML="La marca no puede estar vacia"
-                errMarca.style.color="red"
-            }
-            error=error+1;
-        }else{
-            data["marca"] = marca.value
-            errMarca.innerHTML=""
-        } 
+    if (marca) {
+        if (marca.value === "") {
+            // Si la marca está vacía, marcamos un error
+            mostrarError(errMarca, "La marca no puede estar vacia");
+            error++;
+        } else {
+            data["marca"] = marca.value;
+            ocultarError(errMarca);
+        }
     }
 
-    if(modelo){
-        if(modelo.value==""){
-            //si nombre vacio nombre incorrecto
-            if(errModelo){
-                errModelo.innerHTML="El modelo no puede estar vacio"
-                errModelo.style.color="red"
-            }
-            error=error+1;
-        }else{
-            data["modelo"] = modelo.value
-            errModelo.innerHTML=""
-        } 
-    }
+    // Repite este bloque para otros campos...
 
-    if(anno){
-        if(anno.value=="" || !/^\d+(\.\d+)?$/.test(anno.value)){
-            //si nombre vacio nombre incorrecto
-            if(errAnno){
-                errAnno.innerHTML="El año no puede estar vacio"
-                errAnno.style.color="red"
-            }
-            error=error+1;
-        }else{
-            data["anno"] = anno.value
-            errAnno.innerHTML=""
-        } 
-    }
+    console.log("DATOS:", data);
 
-    if(color){
-        if(color.value==""){
-            //si nombre vacio nombre incorrecto
-            if(errColor){
-                errColor.innerHTML="El color no puede estar vacio"
-                errColor.style.color="red"
-            }
-            error=error+1;
-        }else{
-            data["color"] = color.value
-            errColor.innerHTML=""
-        } 
-    }
-
-    if(caballos){
-        if(caballos.value=="" || !/^\d+(\.\d+)?$/.test(caballos.value)){
-            //si nombre vacio nombre incorrecto
-            if(errCaballos){
-                errCaballos.innerHTML="Formato invalido"
-                errCaballos.style.color="red"
-            }
-            error=error+1;
-        }else{
-            data["caballos"] = caballos.value
-            errCaballos.innerHTML=""
-        } 
-    }
-
-    data['combustible'] = combustible
-
-    if(precio){
-        if(precio.value=="" || !/^\d+(\.\d+)?$/.test(precio.value)){
-            //si nombre vacio nombre incorrecto
-            if(errPrecio){
-                errPrecio.innerHTML="Formato invalido"
-                errPrecio.style.color="red"
-            }
-            error=error+1;
-        }else{
-            data["precio"] = precio.value
-            errPrecio.innerHTML=""
-        } 
-    }
-
-    if(kilometros){
-        if(kilometros.value=="" || !/^\d+(\.\d+)?$/.test(kilometros.value)){
-            //si nombre vacio nombre incorrecto
-            if(errKilometros){
-                errKilometros.innerHTML="Formato invalido"
-                errKilometros.style.color="red"
-            }
-            error=error+1;
-        }else{
-            data["kilometros"] = kilometros.value
-            errKilometros.innerHTML=""
-        } 
-    }
-
-    data['cambio'] = cambio
-
-    console.log("DATOS:", data)
-
-    if(error==0){
-        //si no hay errores
+    if (error === 0) {
+        // Si no hay errores, enviar el formulario
         send_POST_form(server, data);
-    }
-
-    if (error > 0) {
-        // If there are validation errors, prevent form submission
+    } else {
+        // Si hay errores de validación, prevenir la presentación del formulario
         event.preventDefault();
-
-        // Display a user-friendly error message
-        let errorMessage = "Please correct the following errors:\n";
-
-        if (errMarca.style.color !== "green") {
-            errorMessage += "- Invalid marca\n";
-        }
-        if (errModelo.style.color !== "") {
-            errorMessage += "- Invalid modelo\n";
-        }
-        if (errAnno.style.color !== "") {
-            errorMessage += "- Invalid año\n";
-        }
-        if (errColor.style.color !== "") {
-            errorMessage += "- Invalid color\n";
-        }
-        if (errCaballos.style.color !== "") {
-            errorMessage += "- Invalid caballos\n";
-        }
-        if (errPrecio.style.color !== "") {
-            errorMessage += "- Invalid precio\n";
-        }
-        if (errKilometros.style.color !== "") {
-            errorMessage += "- Invalid kilometros\n";
-        }
-
-        alert(errorMessage);
-    }
+        // Mostrar mensajes de error en la página o en una ventana de diálogo
+        mostrarMensajeError("Por favor, corrige los siguientes errores:\n" +
+            "- Marca inválida\n" +
+            "- Modelo inválido\n" +
+            ""
+            // Agrega otros mensajes para campos adicionales...
+        );
+    }    
 }
-function send_POST_form(path, params, method='post') {
 
-    // The rest of this code assumes you are not using a library.
-    // It can be made less verbose if you use one.
+function mostrarError(elemento, mensaje) {
+    elemento.textContent = mensaje;
+    elemento.style.color = "red";
+}
+
+function ocultarError(elemento) {
+    elemento.textContent = "";
+}
+
+function mostrarMensajeError(mensaje) {
+    // Aquí puedes mostrar los mensajes de error en la página o en una ventana de diálogo
+    console.error(mensaje);
+}
+
+function send_POST_form(path, params, method = 'post') {
     const form = document.createElement('form');
     form.method = method;
     form.action = path;
-  
+
     for (const key in params) {
-      if (params.hasOwnProperty(key)) {
-        const hiddenField = document.createElement('input');
-        hiddenField.type = 'hidden';
-        hiddenField.name = key;
-        hiddenField.value = params[key];
-  
-        form.appendChild(hiddenField);
-      }
+        if (params.hasOwnProperty(key)) {
+            const hiddenField = document.createElement('input');
+            hiddenField.type = 'hidden';
+            hiddenField.name = key;
+            hiddenField.value = params[key];
+
+            form.appendChild(hiddenField);
+        }
     }
 
-    
-  
     document.body.appendChild(form);
     form.submit();
 }
